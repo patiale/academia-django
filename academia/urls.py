8c23e3949ff6988f2d8b002bd561a5f7e87ffd54
patialeala@gmail.com <patialeala@gmail.com>
2026-06-26 17:59:36 -0400

    agregada pagina de inicio, configuracion de vistas basada en clases y navegacion

--- academia/urls.py
+++ academia/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Cuando alguien entre a http://127.0.0.1:8000/ se ejecutará la vista 'lista_cursos'
-    path('', views.ListaCursosView.as_view(), name='lista_cursos'),
+    path('', views.InicioView.as_view(), name='inicio'),
+    path('cursos/', views.ListaCursosView.as_view(), name='lista_cursos'),
 ]
--- academia/views.py
+++ academia/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import ListView
 from .models import Curso
+from django.views.generic import TemplateView
 
 class ListaCursosView(ListView):
     model = Curso
     template_name = 'academia/lista_cursos.html'  # El HTML que va a renderizar
     context_object_name = 'cursos'                # El nombre de la variable en el HTML
+
+class InicioView(TemplateView):
+    template_name = 'academia/inicio.html'
